9d50cfcf6c817e67c46404aee07825ae72fb6eed
Zameer Ali Tunio <64825646+Zameer-Ali-Tunio@users.noreply.github.com>
2021-05-30 13:44:43 +0500

    login page flutter assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -8,8 +8,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: (Scaffold(body: Text("Zameer ali tunio"))),
-      
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Login Page")),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 100),
+              Container(
+                  width: 250,
+                  child: TextField(
+                    decoration:
+                        InputDecoration(hintText: 'Enter your Email or phone'),
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  width: 250,
+                  child: TextField(
+                    decoration: InputDecoration(hintText: '**********'),
+                    obscureText: true,
+                  )),
+              SizedBox(height: 20),
+              FlatButton(
+                color: Colors.blue, // background
+                textColor: Colors.white,
+                focusColor: Colors.red,
+                hoverColor: Colors.green,
+                onPressed: () {},
+                child: Text('Sign In'),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
